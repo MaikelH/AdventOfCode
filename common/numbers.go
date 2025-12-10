8536ab2ac49e426627c73839fae5631c8fe30e69
Maikel Hofman <m.hofman@chronos-technology.nl>
2025-12-10 15:16:20 +0100

    2025 day 2 part 2 (wip)

--- common/numbers.go
+++ common/numbers.go
@@ -1,5 +1,10 @@
 package common
 
+import (
+	"math"
+	"sort"
+)
+
 type Number interface {
 	int | float32 | float64
 }
@@ -19,4 +24,37 @@ func IsMonotonic[T Number](slice []T) bool {
 	}
 
 	return isIncreasing || isDecreasing
+}
+
+// Divisors returns a slice containing all divisors of a given integer n, sorted in ascending order.
+// If n is 0, it returns an empty slice. Includes both positive and negative divisors if n is negative.
+func Divisors(n int) []int {
+	if n == 0 {
+		return []int{}
+	}
+	m := n
+	if m < 0 {
+		m = -m
+	}
+	small := []int{}
+	large := []int{}
+
+	limit := int(math.Sqrt(float64(m)))
+	for i := 1; i <= limit; i++ {
+		if m%i == 0 {
+			small = append(small, i)
+			if i != m/i { // avoid duplicate when i*i == m
+				large = append(large, m/i)
+			}
+		}
+	}
+
+	// Merge: small is ascending; large is descending, so reverse large
+	for i, j := 0, len(large)-1; i < j; i, j = i+1, j-1 {
+		large[i], large[j] = large[j], large[i]
+	}
+	res := append(small, large...)
+	// Optional: sort to be safe if order matters
+	sort.Ints(res)
+	return res
 }
--- year2025/Day2.go
+++ year2025/Day2.go
@@ -1,6 +1,7 @@
 package year2025
 
 import (
+	"AoC/common"
 	"strconv"
 	"strings"
 )
@@ -37,5 +38,37 @@ func (d Day2) SolvePart1(input string) (int64, error) {
 }
 
 func (d Day2) SolvePart2(input string) (int64, error) {
-	return int64(0), nil
+	ranges := strings.Split(input, ",")
+	ids := make([][]int, len(ranges))
+	for i, r := range ranges {
+		split := strings.Split(r, "-")
+		start, _ := strconv.Atoi(split[0])
+		end, _ := strconv.Atoi(split[1])
+		ids[i] = []int{start, end}
+	}
+
+	sum := int64(0)
+	for _, startEnd := range ids {
+		for i := startEnd[0]; i <= startEnd[1]; i++ {
+			idString := strconv.Itoa(i)
+			length := len(idString)
+			divisors := common.Divisors(length)
+
+			for _, divisor := range divisors {
+				if divisor == length {
+					continue
+				}
+				parts := make(map[string]struct{}, divisor+1)
+				for i := 0; i < length/divisor; i++ {
+					parts[idString[i:i+divisor]] = struct{}{}
+				}
+
+				if len(parts) == 1 {
+					sum += int64(i)
+				}
+			}
+		}
+	}
+
+	return sum, nil
 }
